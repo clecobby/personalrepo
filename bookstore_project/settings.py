5da50c3793250dc5c9435a48fab541ca7b9e76d1
blackmamba <blackmamba@localhost.localdomain>
2020-02-25 17:28:15 +0000

    ch6

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third-party
+    'crispy_forms', #new
+    #local
     'users.apps.UsersConfig', 
     'pages.apps.PagesConfig',#new
 ]
+CRISPY_TEMPLATE_PACK='bootstrap4'#new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,8 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),] #new
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')#new
 
-
+STATICFILES_FINDERS=[
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+]
 AUTH_USER_MODEL='users.CustomUser' 
-LOGIN_REDIRECT_URL='home' #new
-LOGOUT_REDIRECT_URL='home'#new
+LOGIN_REDIRECT_URL='home' 
+LOGOUT_REDIRECT_URL='home'

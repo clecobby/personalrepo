46cca32599ad03386c6e5956dff381fc7225ad9f
blackmamba <blackmamba@localhost.localdomain>
2020-03-04 05:26:58 +0000

    ch11

--- books/models.py
+++ books/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import uuid #new
 from django.urls import reverse
+from django.contrib.auth import get_user_model #n32
 
 # Create your models here.
 class Book (models.Model):
-    id=models.UUIDField(#new
+    id=models.UUIDField(
     primary_key=True,
     default=uuid.uuid4,
     editable=False)
@@ -15,5 +16,22 @@ class Book (models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self): #new
-        return reverse('book_detail',args=[str(self.id)])
+    def get_absolute_url(self): 
+        return reverse('book_detail',kwargs={'pk': str(self.pk)})
+
+class Review(models.Model):# new
+    book=models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+
+    review=models.CharField(max_length=255)
+    author=models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return self.review
+        
